1fda8bdbb353030680b8afd2b0f994e4b99ab3ea
benzed3 <benzed3@gmail.com>
2019-03-11 18:04:20 -0400

    okay this is final push

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -42,28 +42,20 @@ $("#submit").on("click", function (event) {
 database.ref().on("child_added", function (childSnapshot) {
 
     var newFreq = $("#frequency-input").val();
-    console.log(newFreq);
 
     var newTime = $("#time-input").val();
-    console.log(time);
 
     var timeConverted = moment(newTime, "HH:mm").subtract(1, "years");
-    console.log(timeConverted);
 
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     var diffTime = moment().diff(moment(timeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
 
     var tRemainder = diffTime % newFreq;
-    console.log(tRemainder);
 
     var minutes = newFreq - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + minutes);
 
     var arrival = moment().add(minutes, "minutes");
-    console.log("ARRIVAL TIME: " + moment(arrival).format("hh:mm"));
 
     $("#border").append("<div class='table'><table><tr><td> " +
         childSnapshot.val().train +
